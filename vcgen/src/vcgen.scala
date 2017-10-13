4ea8c899528fb87d49688d4dcd6dad57e2c0cedd
Devin H <devinhilly@gmail.com>
2017-10-13 13:33:14 -0400

    Attempting translation from code to guarded commands

--- vcgen/src/vcgen.scala
+++ vcgen/src/vcgen.scala
@@ -66,6 +66,15 @@ object VCGen {
   type Program = Product4[String, Preconditions, Postconditions, Block]
 
 
+  /* Loop-free guarded commands.*/
+  trait GuardedCommand
+
+  case class Assume(a: Assertion) extends GuardedCommand
+  case class Assert(b: Assertion) extends GuardedCommand
+  case class Havoc(x: String) extends GuardedCommand
+  case class Concat(c1: GuardedCommand, c2: GuardedCommand) extends GuardedCommand
+  case class Rect(c1: GuardedCommand, c2: GuardedCommand) extends GuardedCommand
+
   object ImpParser extends RegexParsers {
     /* General helpers. */
     def pvar  : Parser[String] = "\\p{Alpha}(\\p{Alnum}|_)*".r
@@ -165,6 +174,97 @@ object VCGen {
       ("program" ~> (pvar ~ rep("pre" ~> assn) ~ rep("post" ~> assn)) <~ "is") ~ (block <~ "end") ^^ {
         case n ~ pre ~ post ~ b => (n, pre, post, b)
       }
+  }
+
+  def replace(e: ArithExp, x: String, tmp: String): ArithExp = {
+    // return e with all instances of x replaced with tmp
+    return e
+  }
+
+  /* Translate an Assign statement into guarded commands. */
+  def GCAssign(statement: Statement): GuardedCommand = {
+    // GC(x := e) = assume tmp = x; havoc x; assume (x = e[tmp/x]);
+    var x = statement.x
+    var e = statement.value
+    var tmp
+    return Concat(Assume(Bcmp(tmp, "=", x)), 
+          Concat(Havoc(x), Assume(Bcmp(x, "=", replace(e, x, tmp)))))
+  }
+
+  /* Translate a Write statement into guarded commands. */
+  def GCWrite(statement: Statement): GuardedCommand = {
+    // do translation here.
+  }
+
+  /* Translate a ParAssign statement into guarded commands. */
+  def GCParAssign(statement: Statement): GuardedCommand = {
+    // GC(x1, x2 := e1, e2) = assume tmp1 = x1; assume tmp2 = x2; havoc x1; havoc x2;
+    // assume (x1 = e1[tmp1/x1]); assume (x2 = e2[tmp2/x2]);
+    var x1 = statement.x1
+    var x2 = statement.x2
+    var e1 = statement.value1
+    var e2 = statement.value2
+    var tmp1
+    var tmp2
+    return Concat(Assume(Bcmp(tmp1, "=", x1)), 
+          Concat(Assume(Bcmp(tmp2, "=", x2)), Concat(Havoc(x1), Concat(Havoc(x2),
+          Concat(Assume(Bcmp(x1, "=", replace(e1, x1, tmp1))), 
+          Assume(Bcmp(x2, "=", replace(e2, x2, tmp2))))))))
+  }
+
+  /* Translate an If statement into guarded commands. */
+  def GCIf(statement: Statement): GuardedCommand = {
+    // GC(if b then c1 else c2) = (assume b; GC(c1)) [] (assume !b; GC(c2))
+    var b = statement.cond
+    var c1 = statement.th
+    var c2 = statement.el
+    return Rect(Concat(Assume(b), GC(c1)), Concat(Assume(BNot(b)), GC(c2)))
+  }
+
+  /* Translate a While statement into guarded commands. */
+  def GCWhile(statement: Statement): GuardedCommand = {
+    // GC({I} while b do c) = assert I; havoc x1; ...; havoc xn; assume I;
+    // (assume b; GC(c); assert I; assume false) [] assume !b
+    var I = statement.inv
+    var b = statement.cond
+    var c = statement.body
+    var havocs = havocVars(c) // Concat(havoc(x), havoc(y))
+    return Concat(Assert(I), Concat(havocs, Concat(Assume(I), 
+          Rect(Concat(Assume(b), Concat(GC(c), Assert(I))), Assume(Bnot(b))))))
+  }
+
+  /* Translates each statement in the block into a loop-free guarded command. */
+  def GC(block: Block): GuardedCommand = {
+    GuardedCommand gc = Assume(true)
+    for (statement <- block) {
+      if (statement.getClass == Assign) {
+        gc = Concat(gc, GCAssign(statement))
+      } else if (statement.getClass == Write) {
+        gc = Concat(gc, GCWrite(statement))
+      } else if (statement.getClass == ParAssign) {
+        gc = Concat(gc, GCParAssign(statement))
+      } else if (statement.getClass == If) {
+        gc = Concat(gc, GCIf(statement))
+      } else if (statement.getClass == While) {
+        gc = Concat(gc, GCWhile(statement))
+      }
+    }
+    return gc
+  }
+
+  /* Returns the program in loop-free guarded commands. */
+  def computeGC(program: Product4): Product2 = {
+    // want to return (programName, c_H)
+    GuardedCommand command = GC(program[3])
+    var precond
+    for (pre <- program[1]) {
+      precond = Concat(precond, pre)
+    } 
+    command = Concat(precond, command)
+    for (post <- program[2]){
+      command = Concat(command, post)
+    }
+    return (program[0], command)
   }
 
   def main(args: Array[String]): Unit = {
