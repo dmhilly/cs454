edf7b758c52f85fc7e5a1df136febacc389c39bd
Kate Rogers <Kate@Kates-MacBook-Pro-6.local>
2017-10-17 21:51:39 -0400

    Hey

--- vcgen/src/vcgen.scala
+++ vcgen/src/vcgen.scala
@@ -6,6 +6,8 @@ import java.io.FileReader
 // 2. figure out all this type stuff D:
 // 3. translate guarded commands into verification condition
 
+//hey
+
 object VCGen {
 
   /* Arithmetic expressions. */
